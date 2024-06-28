a9bba4d9a7f5542af8fd96d3c79943b1b402fdc7
Vlad <Vladroon22@mail.ru>
2024-06-28 19:19:17 +0300

    Update shortener.go

--- internal/shortener/shortener.go
+++ internal/shortener/shortener.go
@@ -1,6 +1,21 @@
 package shortener
 
+import (
+	"math/rand"
+	"time"
+)
+
 type URL struct {
 	OriginalURL string `json:"original"`
 	ShortURL    string `json:"short"`
+}
+
+func MakeShort() string {
+	rand.Seed(time.Now().UnixNano())
+	alphabet := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()")
+	short := make([]rune, 6)
+	for rn := range short {
+		short[rn] = alphabet[rand.Intn(len(alphabet))]
+	}
+	return string(short)
 }
